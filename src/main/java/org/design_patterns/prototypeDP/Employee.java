368be1e78e67ead35750a66421d5c18cb581220f
vivekverma1221998 <vivekverma1221998@gmail.com>
2025-04-05 22:37:15 +0530

    prototype-dp

---
+++ src/main/java/org/design_patterns/prototypeDP/Employee.java
@@ -0,0 +1,32 @@
+package org.design_patterns.prototypeDP;
+
+public class Employee implements Cloneable{
+
+    private String name;
+
+    private int sal;
+
+    private String department;
+
+
+    public Employee(String name,int sal , String department) throws InterruptedException {
+        Thread.sleep(5000);
+        this.name = name;
+        this.sal = sal;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", sal=" + sal +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return this;
+    }
+}
---
+++ src/main/java/org/design_patterns/prototypeDP/Main.java
@@ -0,0 +1,26 @@
+package org.design_patterns.prototypeDP;
+
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException, CloneNotSupportedException {
+        Employee emp = new Employee("vivek" , 1000, "cse");
+
+        System.out.println(emp);
+
+        /*
+        The Prototype Design Pattern is a creational pattern that is used when
+        creating an object is costly or complex.
+        Instead of creating a new object from scratch,
+        we clone an existing object (the prototype) and modify it if needed.
+         */
+        Employee emp2 = (Employee) emp.clone();
+        System.out.println(emp2);
+
+
+
+
+
+
+    }
+}
