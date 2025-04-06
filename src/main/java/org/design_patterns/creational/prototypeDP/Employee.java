2cb61a285ecdbcd89282f2859a119bf20709f80b
vivekverma1221998 <vivekverma1221998@gmail.com>
2025-04-06 13:16:24 +0530

    prototype design pattern of deep copy

--- src/main/java/org/design_patterns/creational/prototypeDP/Employee.java
+++ src/main/java/org/design_patterns/creational/prototypeDP/Employee.java
@@ -1,19 +1,43 @@
 package org.design_patterns.creational.prototypeDP;
 
-public class Employee implements Cloneable{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee implements Cloneable {
 
     private String name;
-
     private int sal;
-
     private String department;
+    private List<String> list = new ArrayList<>();
 
-
-    public Employee(String name,int sal , String department) throws InterruptedException {
-        Thread.sleep(5000);
+    public Employee(String name, String department) {
         this.name = name;
-        this.sal = sal;
         this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSal(int sal) throws InterruptedException {
+        Thread.sleep(3000); // Simulate expensive operation
+        this.sal = sal;
+    }
+
+    public int getSal() {
+        return sal;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public List<String> getList() {
+        return list;
     }
 
     @Override
@@ -22,11 +46,16 @@ public class Employee implements Cloneable{
                 "name='" + name + '\'' +
                 ", sal=" + sal +
                 ", department='" + department + '\'' +
+                ", list=" + list +
                 '}';
     }
 
+    // Deep clone
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        return this;
+        Employee cloned = (Employee) super.clone();
+        // Deep copy of list
+        cloned.list = new ArrayList<>(this.list);
+        return cloned;
     }
 }
--- src/main/java/org/design_patterns/creational/prototypeDP/Main.java
+++ src/main/java/org/design_patterns/creational/prototypeDP/Main.java
@@ -1,13 +1,24 @@
 package org.design_patterns.creational.prototypeDP;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 
     public static void main(String[] args) throws InterruptedException, CloneNotSupportedException {
-        Employee emp = new Employee("vivek" , 1000, "cse");
+        System.out.println("creating object for employee");
+        Employee emp = new Employee("vivek" , "cse");
+        emp.setSal(1000);
+        List<String> ls = new ArrayList<>();
+        ls.add("java");
+        ls.add("c++");
+        ls.add("python");
+        emp.setList(ls);
+        System.out.println("object created for employee");
 
         System.out.println(emp);
-
         /*
         The Prototype Design Pattern is a creational pattern that is used when
         creating an object is costly or complex.
@@ -15,6 +26,16 @@ public class Main {
         we clone an existing object (the prototype) and modify it if needed.
          */
         Employee emp2 = (Employee) emp.clone();
+        System.out.println(emp2);
+
+
+        List<String > al = emp.getList();
+
+        System.out.println("After modification");
+        al.remove(1);
+        emp.setList(al);
+
+        System.out.println(emp);
         System.out.println(emp2);
 
 
